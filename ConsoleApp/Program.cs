c0440d8e9cdc47bc2b355b67cc254548632a636d
jelo <markalaba03730@gmail.com>
2025-07-30 01:37:11 +0000

    i love ado eating adobo

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Mark Alaba");
+        Console.WriteLine("20241112765");
+        Console.WriteLine("crazy but it is okay");
+    }
+}
